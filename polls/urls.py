3ea030983c8aa16d5c64ee73986405e3084c72ac
suyoungkim <suyoung154@gmail.com>
2024-01-14 17:22:35 +0000

    part03 completed

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from polls.views import index
+from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", view=index, name="index"),
+    path("", view=views.index, name="index"),
+    path("<int:question_id>/", view=views.detail, name="detail"),
+    path("<int:question_id>/results/", view=views.results, name="results"),
+    path("<int:question_id>/vote/", view=views.vote, name="vote"),
 ]
